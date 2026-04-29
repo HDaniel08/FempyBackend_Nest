2c2e81b969b9659f750d1db4b5e3c4333d36fea8
HDaniel08 <horvath.daniel.dev@gmail.com>
2026-04-29 14:56:07 +0200

    cors fix

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ app.enableCors({
     origin: [
       'http://localhost:5173',
       'http://127.0.0.1:5173',
+      'https://fempyadmin.horvath-daniel-dev.workers.dev',
     ],
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: [
